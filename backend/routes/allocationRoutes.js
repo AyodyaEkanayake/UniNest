024c62724810021a09f27412cfaadd8af7ecf8ef
Ayodya Ekanayake <ayodyae822@gmail.com>
2026-03-04 21:36:29 +0530

    Delete backend directory

--- backend/routes/allocationRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const allocationController = require("../controllers/allocationController");
-const { verifyToken } = require("../middleware/authMiddleware");
-
-router.get("/my", verifyToken, allocationController.getMyAllocation);
-
-module.exports = router;
--- backend/routes/roomRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const roomController = require("../controllers/roomController");
-
-router.post("/", roomController.createRoom);  // create room
-router.get("/", roomController.getRooms);     // get all rooms
-
-module.exports = router;
